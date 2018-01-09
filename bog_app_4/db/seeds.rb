9709598df3bb3133f62718aefe177de3063ce4e8
Kyle Easton <kyleeaston@Kyles-Air.att.net>
2018-01-09 02:23:16 -0800

    bog_app_4 done

--- bog_app_4/db/seeds.rb
+++ bog_app_4/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Creature.delete_all
+
+seed_some_stuff = [
+  {
+    name: "Yoda",
+    description: "Little green man"
+  },
+  {
+    name: "Luke Skywalker",
+    description: "The last jedi"
+  },
+  {
+    name: "Darth Vader",
+    description: "Doesn't like sand because it gets everywhere"
+  }
+]
+
+seed_some_stuff.each do |creature|
+  Creature.create(creature)
+end
+
+puts "Seeded some star wars dudes"
